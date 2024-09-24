98cf5b393b5d1d79595455fde58bb3483e390eeb
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-24 17:43:23 +0530

    feat: empty string calculation

--- src/components/StringCalculator.tsx
+++ src/components/StringCalculator.tsx
@@ -1,17 +1,30 @@
 import { ChangeEvent, useState } from "react";
+import { add } from "../utils/calculate";
 
 const StringCalculator: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [result, setResult] = useState<number | null>(null);
+
+  const [error, setError] = useState<string | null>("");
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
-  const handleCalculate = () => {};
+  const handleCalculate = () => {
+    try {
+      const sum: number = add(inputValue);
+      setResult(sum);
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
 
   return (
     <div>
       <input type="text" value={inputValue} onChange={handleInputChange} />
+
+      {result && <p>Result: {result}</p>}
 
       <button onClick={handleCalculate}>Calculate</button>
     </div>
